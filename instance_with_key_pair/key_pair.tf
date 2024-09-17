c635312ef67aa8e6a018deb9bfd7a14ab2e3cdf6
vijayrmourya <vijayrmourya@gmail.com>
2024-09-17 12:43:06 +0000

    added module to create ec2 with key pair and security group to ssh

---
+++ instance_with_key_pair/key_pair.tf
@@ -0,0 +1,23 @@
+resource "tls_private_key" "key_for_instance" {
+  algorithm = var.tls_private_key_algorithm
+  rsa_bits  = var.tls_private_key_rsa_bits
+}
+
+resource "aws_key_pair" "key_for_instance" {
+  key_name   = var.aws_key_pair_key_name
+  public_key = tls_private_key.key_for_instance.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  filename = var.local_file_private_key_filename
+  content  = tls_private_key.key_for_instance.private_key_pem
+  file_permission = var.local_file_private_key_file_permission 
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.key_for_instance.key_name
+}
+
+output "private_key_path" {
+  value = local_file.private_key.filename
+}
